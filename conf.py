dc1e328e29594e849f84b6615d6deaf3ee952a6b
Zunder45 <sashaorlov02y@yandex.ru>
2022-11-19 16:24:33 +0500

    added profile

--- conf.py
+++ conf.py
@@ -1,6 +1,17 @@
-#режимы "" - просто удаление каталога, "R" - рекурсивное удаление
-path_dir = [
-  ['/home/qwerty/Pyscpt/test/test1/',''],
-  ['/home/qwerty/Pyscpt/test/test2','R']
-]
+#режимы "" - просто удаление каталога, "R" - рекурсивное удаление каталога (корневой каталог остается)
+# pathDir = {
+#   'def': [
+#     ['Путь',''],
+#     ['...','R']
+#   ],
+#   '1':[['...','']]
+# }
+exceptions = []
 
+pathDir = {
+  'def': [
+    ['/home/qwerty/Pyscpt/test/test1/',''],
+    ['/home/qwerty/Pyscpt/test/test2','R']
+  ],
+  '1':[['/home/qwerty/Pyscpt/test/ffffff/','']]
+}
--- main.py
+++ main.py
@@ -1,34 +1,55 @@
-import os, shutil, time
+import sys, os, shutil, time
 import conf
 
 
-#удаление файлов
-for item in conf.path_dir:
+def getProfile():
 
-  if os.path.exists(item[0]):
-    if item[1] == "": # Простое удаление корневого каталога 
-      fileName = item[0]
-      if os.path.isdir(fileName):
-          shutil.rmtree(fileName)
-          print(fileName, ' уделен!')
-        
-      else:
-        os.remove(fileName)
-        print(fileName)
-      
-    if item[1] == "R": # рекурсивное удаление каталога (корневой каталог остается)
-      for f in os.listdir(item[0]):
-        fileName = os.path.join(item[0],f)
+  args = sys.argv
+  prf = ""
+  index = 0
+
+  for arg in args: #поиск аргументов
+    if arg == "-p":
+      if index != len(args)-1:
+        prf = args[index+1] 
+    index = index + 1
+
+  if prf == "" or prf  not in conf.pathDir:
+    print("Профиль: " + "def")
+    return "def"
+  else: 
+    print("Профиль: " + prf)
+    return prf
+
+
+def delFiles(profile):
+  #удаление файлов
+  for item in conf.pathDir[profile]:
+
+    if os.path.exists(item[0]):
+      if item[1] == "": # Простое удаление корневого каталога 
+        fileName = item[0]
         if os.path.isdir(fileName):
-          shutil.rmtree(fileName)
-          print(fileName, ' уделен!')
-        
+            shutil.rmtree(fileName)
+            print(fileName, ' уделен!')
         else:
           os.remove(fileName)
           print(fileName)
+        
+      if item[1] == "R": # рекурсивное удаление каталога (корневой каталог остается)
+        for f in os.listdir(item[0]):
+          fileName = os.path.join(item[0],f)
+          if os.path.isdir(fileName):
+            shutil.rmtree(fileName)
+            print(fileName, ' уделен!')
+          else:
+            os.remove(fileName)
+            print(fileName)
 
-  
 
-print("OK")
+def main():  
+  delFiles(getProfile())
+  print("OK")
+  # time.sleep(2)
 
-# time.sleep(2)
+main()
